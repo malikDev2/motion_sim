4a49377cc29131bcc0711a0af9e5fccebff4bd3e
Malik <malik.h.jacobs@outlook.com>
2025-07-29 15:28:26 -0400

    created more racer structs

--- src/racer_options.cpp
+++ src/racer_options.cpp
@@ -28,6 +28,40 @@ std::vector<RacerOption> getAllRacerOptions() {
     options.emplace_back("Lamborghini Revuelto", 218f, "img");
     options.emplace_back("Koenigsegg Jesko Absolut", 310f, "img");
     options.emplace_back("Hennessey Venom F5", 311f, "img");
+
+    // Weapons
+    options.emplace_back("Glock 17", 818.0f, "img");
+    options.emplace_back("Remington 870", 1091.0f, "img");
+    options.emplace_back("AR-15", 2045.0f, "img");
+    options.emplace_back("AK-47", 1602.0f, "img");
+    options.emplace_back("M24 Sniper", 1773.0f, "img");
+
+    // Animals
+    options.emplace_back("Cheetah", 65.0f, "img");
+    options.emplace_back("Antelope", 55.0f, "img");
+    options.emplace_back("Lion", 50.0f, "img");
+    options.emplace_back("Greyhound", 45.0f, "img");
+    options.emplace_back("Peregrine Falcon", 240.0f, "img");
+    options.emplace_back("Golden Eagle", 200.0f, "img");
+    options.emplace_back("Red-tailed Hawk", 120.0f, "img");
+    options.emplace_back("Black Marlin", 82.0f, "img");
+    options.emplace_back("Mako Shark", 60.0f, "img");
+    options.emplace_back("Orca Whale", 35.0f, "img");
+    options.emplace_back("Dolphin", 30.0f, "img");
+
+    // Personal Transportation
+    options.emplace_back("City Bike", 15.0f, "img");
+    options.emplace_back("Skateboard", 10.0f, "img");
+    options.emplace_back("Scooter", 8.0f, "img");
+    options.emplace_back("Eletric Scooter", 20.0f, "img");
+    options.emplace_back("Walking", 3.0f, "img");
+    options.emplace_back("Jogging", 7.0f, "img");
+    options.emplace_back("Running", 13.0f, "img");
+    options.emplace_back("Sprinting", 17.0f, "img");
+
+
+
+    
      
 
 
